d2bc0a20dd2b737f05d7dbf99da103921c742507
Your Name <you@example.com>
2023-05-04 17:57:07 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './components/pages/home/Home';
+import MovieList from './components/movielist/MovieList';
+import Movie from './components/movieDetails/MovieDetails';
+import Footer from './components/footer/Footer';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header/>
+        <Routes>
+          <Route index element={<Home/>} />
+          <Route path='movie/:id' element={<Movie/>} />
+          <Route path='movies/:type' element={<MovieList/>} />
+          <Route path='/*' element={<h1>erroer page</h1>} />
+
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Footer.css"
+
+function Footer() {
+  return (
+    <div className="footer">
+    <h3 className="footer-text">Copyright © IMDb Clone</h3>
+  </div>
+  );
+}
+
+
+export default Footer
